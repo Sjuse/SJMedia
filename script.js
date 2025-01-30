7c490e378872185fe9e8b43de73c26c58dccd697
Sjuse <sameemsahibi@gmail.com>
2025-01-30 17:50:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('nav-toggle');
 }
+
+function acceptTerms() {
+    document.getElementById("termsContainer").style.display = "none";
+}
+
+function declineTerms() {
+    alert(
+        "Welcome to SJ Media!\n\n" +
+        "To continue using our website, you must review and accept our Terms of Service. By clicking 'Accept,' you agree to comply with our terms and conditions. If you do not agree, please refrain from using this website.\n\n" +
+        "Thank you for your understanding!"
+    );
+}
+
+// Show the modal when the page loads
+window.onload = function() {
+    termsCondition.style.display = 'flex'; // show the modal
+};
+
+// Check if the user has already accepted the terms
+if (!localStorage.getItem('termsAccepted')) {
+    termsCondition.style.display = 'flex'; //show the modal
+}
+
+// Hide the modal and store acceptance in local storage
+acceptButton.addEventListener('click', function() {
+    termsCondition.style.display = 'none';
+    localStorage.setItem('termsAccepted', 'true');
+});
